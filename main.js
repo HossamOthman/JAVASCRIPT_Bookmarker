87f603fb7f9260c060d1e0ab70ca007bf52fe0ac
Hossam Othman <rowheadrex@gmail.com>
2023-09-17 14:23:12 +0200

    fetch to ui and delete

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function saveBookmark(e) {
     var siteUrl = document.getElementById('siteURL').value;
 
     var bookmark = {
+        id: Date.now(),
         name: siteName,
         url: siteUrl,
     }
@@ -39,5 +40,47 @@ function saveBookmark(e) {
         // re-set it back to local storage... same command above
         localStorage.setItem('bookmarkApp.bookmarks', JSON.stringify(existBookmarks));
     }
+    fetchBookmarks();
+}
 
+// fetch bookmarks to ui
+function fetchBookmarks() {
+    // get bookmarks from local storage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarkApp.bookmarks'));
+
+    // get output id
+    var bookmarkResults = document.getElementById('bookmarksResults');
+
+    // build output
+    bookmarkResults.innerHTML = '';
+    // looping through to get itterations
+    bookmarks.forEach(bookmark => {
+        var name = bookmark.name;
+        var url = bookmark.url;
+        var id = bookmark.id;
+        var toDate = new Date(id).toISOString().split('T')[0];
+
+        bookmarkResults.innerHTML += `  <div class="resultbkground">
+            <h3>${name} <span>added on: ${toDate}</span></h3>
+            <div>
+                <a class="btn visit" target="_blank" href="${url}">Visit</a>
+                <a onclick="deleteBookmark(${id})" class="btn delete" href="#">Delete</a>
+            </div>
+            </div>`;
+    });
+
+
+}
+
+function deleteBookmark(id) {
+   // get bookmarks from local storage
+   var existBookmarks = JSON.parse(localStorage.getItem('bookmarkApp.bookmarks'));
+   existBookmarks.forEach(bookmark => {
+    if (bookmark.id === id) {
+        existBookmarks.splice(bookmark, 1)
+    }
+    // re-set it back to local storage again... after deletion
+    localStorage.setItem('bookmarkApp.bookmarks', JSON.stringify(existBookmarks));
+    fetchBookmarks();
+   })
 }
